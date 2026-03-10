4b1f8126071d6b9d8ab1518aedd75215ec687740
Kana Ki <kana@ffxivvenues.com>
2026-03-10 03:52:50 +0000

    Add visited check icon to normal card

--- src/components/filterMenu/filters/ratingFilters.ts
+++ src/components/filterMenu/filters/ratingFilters.ts
@@ -3,7 +3,7 @@ import {favouritesService} from "@/lib/services/favouritesService.ts";
 import {visitedService} from "@/lib/services/visitedService.ts";
 
 export const ratingFilters: FilterOption[] = [
-  { name: "Is Favorited", filter: (v) => favouritesService.isFavourite(v.id) },
+  { name: "Favorited", filter: (v) => favouritesService.isFavourite(v.id) },
   { name: "Visited", filter: (v) => visitedService.isVisited(v.id) },
   { name: "Not visited", filter: (v) => !visitedService.isVisited(v.id) }
 ];
