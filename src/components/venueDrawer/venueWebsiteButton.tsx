50deee2f278b66a726b7ce9ef797d78a503f5456
Oreo <232652342+devoreofox@users.noreply.github.com>
2026-04-09 23:59:21 -0300

    fixed nativeButton errors

--- src/components/venueDrawer/venueWebsiteButton.tsx
+++ src/components/venueDrawer/venueWebsiteButton.tsx
@@ -4,7 +4,7 @@ import {LinkIcon} from "lucide-react";
 
 export const VenueWebsiteButton = ({website}: { website: Nullable<string> }) =>
   website &&
-  <Button className="cursor-pointer flex items-center h-10 px-4 gap-2 font-bold grow" render={(props) =>
+  <Button nativeButton={false} className="cursor-pointer flex items-center h-10 px-4 gap-2 font-bold grow" render={(props) =>
     <a href={website} target="_blank" rel="noopener noreferrer" {...props}>
       <LinkIcon className="size-5" strokeWidth={3} stroke="black"/> Website
     </a>
