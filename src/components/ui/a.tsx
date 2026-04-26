8258b9f26345e17ef99d2e1e25e75d550a98624f
devoreofox <232652342+devoreofox@users.noreply.github.com>
2026-04-26 01:17:57 -0300

    Removed the car >:(

--- src/components/ui/a.tsx
+++ src/components/ui/a.tsx
@@ -2,7 +2,6 @@ import React, {type ReactNode} from "react";
 
 export const A = ({ href, children }: { href: string, children: ReactNode }) => (
     <a href={href} className="underline underline-offset-2 hover:opacity-70 transition-opacity" target="_blank" rel="noopener noreferrer">
-
         {children}
     </a>
 );
