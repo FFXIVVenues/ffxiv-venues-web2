ef553cf9c761b18811f7aa00cacde34fdda10360
Kana Ki <kana@ffxivvenues.com>
2026-06-26 00:58:44 +0100

    Add terms of service

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,6 +2,7 @@ import {Route, BrowserRouter, Routes} from "react-router";
 import {VenueDirectoryPage} from "@/pages/venueDirectoryPage/venueDirectoryPage.tsx";
 import {NotFoundPage} from "@/pages/notFoundPage/notFoundPage.tsx";
 import {PrivacyPolicyPage} from "@/pages/legal/privacyPolicyPage.tsx";
+import {TermsOfServicePage} from "@/pages/terms/termsOfServicePage.tsx";
 
 
 export const App = () =>
@@ -10,6 +11,7 @@ export const App = () =>
         <Route index element={<VenueDirectoryPage />} />
         <Route path="/venue/:venueId" element={<VenueDirectoryPage />} />
         <Route path="/privacy" element={<PrivacyPolicyPage />} />
+        <Route path="/terms" element={<TermsOfServicePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
