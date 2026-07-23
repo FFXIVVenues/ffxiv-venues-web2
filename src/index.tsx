01d594ff8a42ff80edb2b77c36e0f625bba88ae4
devoreofox <232652342+devoreofox@users.noreply.github.com>
2026-07-22 22:45:29 -0300

    Swap to SWC and add Lingui

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { i18n } from "@lingui/core";
+import { I18nProvider } from "@lingui/react";
 import { App } from "./components/app/app.tsx";
+import { messages } from "./locales/en/messages.po";
 import "./index.css";
 import { provideTheme } from "@/lib/services/settings/usetheme";
 
 provideTheme();
 
+i18n.load("ja", messages);
+i18n.activate("ja");
+
 const elem = document.getElementById("app-root")!;
 const app = (
-  <StrictMode>
-      <App />
-  </StrictMode>
+    <StrictMode>
+        <I18nProvider i18n={i18n}>
+            <App />
+        </I18nProvider>
+    </StrictMode>
 );
 
 if (import.meta.hot) {
-  const root = (import.meta.hot.data.root ??= createRoot(elem));
-  root.render(app);
+    const root = (import.meta.hot.data.root ??= createRoot(elem));
+    root.render(app);
 } else {
-  createRoot(elem).render(app);
+    createRoot(elem).render(app);
 }
 
--- src/pages/notFoundPage/notFoundPage.tsx
+++ src/pages/notFoundPage/notFoundPage.tsx
@@ -1,10 +1,11 @@
 import {DefaultPageLayout} from "@/pageLayoutss/defaultPageLayout.tsx";
+import {Trans} from "@lingui/react/macro";
 
 export const NotFoundPage = () => {
   return <DefaultPageLayout
     title="Page not found - FFXIV Venues">
     <DefaultPageLayout.Page>
-      Sorry, that page could not be found.
+      <Trans>Sorry, that page could not be found.</Trans>
     </DefaultPageLayout.Page>
   </DefaultPageLayout>
 }
